78e6d1d10e68f7945f23b779cb53dd3bba7dfea6
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2017-03-01 10:47:16 +0800

    ci: libosmogsm is now exporting these fields
    We don't need to patch this file anymore. Let's see if it works out.

--- build_libosmocore.sh
+++ build_libosmocore.sh
@@ -5,9 +5,6 @@ git clone --depth=1 git://git.osmocom.org/libosmocore
 cd libosmocore
 autoreconf --install --force
 
-# hack... need to fix this later..
-sed -i s,"comp128;","comp128v1;comp128v2;comp128v3;", src/gsm/libosmogsm.map
-
 export PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig/
 CC="gcc -m32" ./configure --disable-pcsc --prefix=$DEST
 CC="gcc -m32" make -j 8 install
