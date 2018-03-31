b775f1766f4f8ce664dd99abe46cf9e4efc93da2
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2018-03-31 15:12:49 +0100

    libosmocore: Linking to gnutls is broken but let's ignore it for now
    Somehow the build doesn't attempt to link to GNUtls but as we require no
    random number from libosmocore we can just disable it.

--- build_libosmocore.sh
+++ build_libosmocore.sh
@@ -6,5 +6,5 @@ cd libosmocore
 autoreconf --install --force
 
 export PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig/
-CC="gcc -m32" ./configure --disable-pcsc --prefix=$DEST
+CC="gcc -m32" ./configure --disable-pcsc --prefix=$DEST --disable-gnutls
 CC="gcc -m32" make -j 8 install
